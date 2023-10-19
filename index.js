aa1b8158478fa0be195859666871433e3f955274
Litreum <mirsieolien@gmail.com>
2023-10-19 11:25:16 +0700

    Else condition

--- index.js
+++ index.js
@@ -66,6 +66,23 @@ app.get("/users/:id", (req, res) => {
     res.json(result);
 });
 
+app.get("/users/:id", (req, res) => {
+    // get data dari parameter
+    let id = parseInt(req.params.id);
 
+    // get data dari database
+    let result;
+    const user = data_users.find((user) => user.id === id);
+    if (user) {
+        result = {
+            status: 200,
+            data: user,
+        };
+    } else {
+        res.status(404).json({ error: "User not found" });
+    }
+    res.json(result);
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
+    
