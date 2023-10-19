29b42b0f772cd86b448709131a108606eb8f590f
Litreum <mirsieolien@gmail.com>
2023-10-19 10:37:58 +0700

    UNderstable have a tea

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3333
 
 // GET method
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.json({ message: "Hello, this is a JSON response" });
 })
 
@@ -23,7 +23,7 @@ app.delete('/user', (req, res) => {
     res.send('Got a DELETE request at /user')
 })
 
-app.get('/EsKopi',(req,res) =>{
+app.get('/EsKopi', (req, res) => {
     res.send("Hello ini about")
 })
 
@@ -31,4 +31,22 @@ app.get("/Eskopi/:Vanilla", (req, res) => {
     res.send("Ini tampilan user");
 });
 
-app.listen(port,() => console.log(`Server running on port ${port}`))
+const data_users = [
+    { id: 1, name: "Ilham Anugrah", alamat: "Sukabumi" },
+    { id: 2, name: "Ghina Khairunnisa", alamat: "Bandung" },
+    { id: 3, name: "Hana Syifa", alamat: "Jakarta" },
+    { id: 4, name: "Daehan Ibrahim", alamat: "Bandung" },
+];
+
+    app.get("/users", (req, res) => {
+        //mendapatkan data dari database
+        const data = data_users; // understablee
+        let result = {
+            status: 200,
+            data: data,
+        };
+
+        res.json(result);
+    });
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
